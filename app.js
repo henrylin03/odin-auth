44b650ab66a9ae919c76570ce56f0092d1da49ff
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-05 13:41:22 +1100

    feat: add .env file with required db details and delete the separate db/pool.js file
    instructions on odin just had pool within the app.js for simplicity

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
-const express = require("express");
 const path = require("node:path");
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+require("dotenv").config();
+
+const pool = new Pool({
+	host: "localhost",
+	user: process.env.USER_NAME,
+	database: "odinauthbasics",
+	password: process.env.PASSWORD,
+	port: 5432,
+});
 
 const app = express();
-
-app.use(express.static(path.join(__dirname, "public")));
-
-/* set up EJS templating */
-app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-/* middleware to parse data in request body */
-app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (_req, res) => res.render("index"));
 
-const PORT = 3000;
-app.listen(PORT, (error) => {
-	if (error) throw Error;
-	console.log(`Listening on port ${PORT}`);
+app.listen(3000, (error) => {
+	if (error) {
+		throw error;
+	}
+	console.log("app listening on port 3000!");
 });

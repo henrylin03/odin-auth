bea493ee9084e11e7473efdeb9867a0410c51af8
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-05 16:40:07 +1100

    Squashed commit of the following:
    commit a22537815363c391e4fbf3a9b49b1ce4b1c69cb6
    Author: Henry Lin <83106787+henrylin03@users.noreply.github.com>
    Date:   Thu Feb 5 14:28:16 2026 +1100
    
        feat(app.js): add a POST route and controller method to enable login
    
        the 'controller' here is authenticating using passport.authenticate, and defining success and failure redirect paths too
    
    commit 8c8e4ddd7bd46f7d6ee78b6f4595b1b370a4bec5
    Author: Henry Lin <83106787+henrylin03@users.noreply.github.com>
    Date:   Thu Feb 5 14:25:48 2026 +1100
    
        feat(index.ejs): add login form to home page
    
    commit 73c77aca36008c2f891e8ba70da777f811caf893
    Author: Henry Lin <83106787+henrylin03@users.noreply.github.com>
    Date:   Thu Feb 5 14:22:43 2026 +1100
    
        feat: add serialise and deserialise functions
    
        iiuc, serialise user takes callback for storing data into the session data, and deserialise then is called on retrieval of the session, extracting the serialised data and attaches user object (from db) to the req.user, which then becomes usable for rest of request, and the app, for the rest of the session, knows that user is 'logged in'
    
    commit f7ed010040a26f2f45e98ac688a3d2a7cb027522
    Author: Henry Lin <83106787+henrylin03@users.noreply.github.com>
    Date:   Thu Feb 5 14:08:33 2026 +1100
    
        feat: set up LocalStrategy
    
        this is the function that ets called when we use passport.authenticate(), taking in username and password, tries to find user in db, and makes sure the password matches. this is bit like a middleware. done() doesn't need to be supplied because we won't be calling this function directly.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
-// const LocalStrategy = require("passport-local").Strategy;
+const LocalStrategy = require("passport-local").Strategy;
 require("dotenv").config();
 
 const pool = new Pool({
@@ -22,7 +22,56 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
+passport.use(
+	new LocalStrategy(async (username, password, done) => {
+		try {
+			const { rows } = await pool.query(
+				"SELECT * FROM users WHERE username = $1",
+				[username],
+			);
+			const user = rows[0];
+
+			if (!user) {
+				return done(null, false, { message: "Incorrect username" });
+			}
+			if (user.password !== password) {
+				return done(null, false, { message: "Incorrect password" });
+			}
+			return done(null, user);
+		} catch (err) {
+			return done(err);
+		}
+	}),
+);
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+	try {
+		const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+			id,
+		]);
+		const user = rows[0];
+
+		done(null, user);
+	} catch (err) {
+		done(err);
+	}
+});
+
+/* ROUTES */
 app.get("/", (_req, res) => res.render("index"));
+
+app.post(
+	"/log-in",
+	passport.authenticate("local", {
+		successRedirect: "/",
+		failureRedirect: "/",
+	}),
+);
+
 app.get("/sign-up", (_req, res) => res.render("sign-up-form"));
 app.post("/sign-up", async (req, res, next) => {
 	try {
